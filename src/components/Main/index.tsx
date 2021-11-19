b34cf6fe300bce6b465df599db127bdc2f073cb6
vitorSantanaDev <vitorsantana.developer@gmail.com>
2021-11-19 17:51:28 -0300

    🎨 Created Main page content

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,8 +1,21 @@
+import * as S from './styles'
+
 const Main = () => {
   return (
-    <main>
-      <h2>Next JS</h2>
-    </main>
+    <S.Wrapper>
+      <S.Logo
+        src="/images/logo.svg"
+        alt="Imagem de um atômo e React Avançado ao lado"
+      />
+      <S.Title>React Avançado</S.Title>
+      <S.Description>
+        Typescript, React JS, Next JS and Styled Components
+      </S.Description>
+      <S.Ilustration
+        src="/images/hero-illustration.svg"
+        alt="Ilustração do site, com um desenvolvedor de frente pro código "
+      />
+    </S.Wrapper>
   )
 }
 
