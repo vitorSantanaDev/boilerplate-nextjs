e1bf073f152c8384e300f38c7a8714649b407659
vitorSantanaDev <vitorsantana.developer@gmail.com>
2021-11-19 20:53:40 -0300

    ✨ Add new features

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
-module.exports = {
-  reactStrictMode: true,
-}
+const withPWA = require('next-pwa')
+
+const isProduction = process.env.NODE_ENV === 'production'
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    disable: !isProduction
+  }
+})
