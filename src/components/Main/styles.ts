1853bcb820366fe1a62d2586df5be46137ee3c79
vitorSantanaDev <vitorsantana.developer@gmail.com>
2021-11-19 17:52:04 -0300

    💄 Added Main page style

---
+++ src/components/Main/styles.ts
@@ -0,0 +1,33 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.main`
+  width: 100%;
+  height: 100%;
+  color: #fff;
+  display: flex;
+  padding: 3rem;
+  text-align: center;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+  background-color: #06092b;
+`
+
+export const Logo = styled.img`
+  width: 25rem;
+  margin-bottom: 2rem;
+`
+
+export const Title = styled.h1`
+  font-size: 2.5rem;
+`
+
+export const Description = styled.h2`
+  font-size: 2rem;
+  font-weight: 400;
+`
+
+export const Ilustration = styled.img`
+  margin-top: 3rem;
+  width: min(30rem, 100%);
+`
